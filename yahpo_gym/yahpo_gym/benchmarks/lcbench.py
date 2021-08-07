ec5b770f81880fb2538440f52dc3b008d3be79b4
pfistfl <pfistererf@googlemail.com>
2021-08-07 16:30:31 +0200

    More try and error

--- yahpo_gym/yahpo_gym/benchmarks/lcbench.py
+++ yahpo_gym/yahpo_gym/benchmarks/lcbench.py
@@ -3,6 +3,7 @@ from yahpo_gym.configuration import config_dict, cfg
 _lcbench_dict = {
     'config_id' : "lcbench",
     'y_names' : ['time', 'val_accuracy', 'val_cross_entropy', 'val_balanced_accuracy', 'test_cross_entropy', 'test_balanced_accuracy'],
+    'y_minimize' : [True, False, True, False, True, False],
     'cont_names': ['epoch', 'batch_size', 'learning_rate', 'momentum', 'weight_decay', 'num_layers', 'max_units', 'max_dropout'],
     'cat_names': ['OpenML_task_id'],
     'fidelity_params': ["epoch"],
