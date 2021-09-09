e7173d060d6ca9af911a337cc336683715272cd3
sumny <lennart.sch@web.de>
2021-09-09 16:44:10 +0200

    some more docu and tests for transfomers

--- yahpo_train/tests/test_cont_scalers.py
+++ yahpo_train/tests/test_cont_scalers.py
@@ -1,15 +1,22 @@
+import pytest
 import torch
 import numpy as np
 from yahpo_train.cont_scalers import *
 
 def test_cont_scaler():
+    def cont_scaler_helper(x, transformer, eps = 1e-5):
+        cr = transformer(x)
+        x_tf = cr(x)
+        x_rec = cr.invert(x_tf)
+        assert torch.max(torch.abs(x_rec - x)).numpy() < eps
+
     x = torch.rand(100)
 
-    cr = ContTransformerLogRange(x)
-    x_tf = cr(x)
-    x_rec = cr.invert(x_tf)
+    for transformer in [ContTransformerNone, ContTransformerRange, ContTransformerNegExpRange, ContTransformerLogRange, ContTransformerMultScalar]:
+        cont_scaler_helper(x, transformer)
 
-    assert torch.max(torch.abs(x_rec - x)).numpy() < 1e-5
+    for transformer in [ContTransformerNone, ContTransformerRange, ContTransformerNegExpRange, ContTransformerMultScalar]:
+        cont_scaler_helper(-x, transformer)
 
 if __name__ == '__main__':
     test_cont_scaler()
