71e9b2a2ee8d09d25e955c7c588acc981b9f67e1
pfistfl <pfistererf@googlemail.com>
2021-09-29 19:22:13 +0200

    bump

--- yahpo_gym/docs/source/conf.py
+++ yahpo_gym/docs/source/conf.py
@@ -6,15 +6,18 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
+import sphinx_bootstrap_theme
+import yahpo_gym
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../yahpo_gym'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +27,7 @@ copyright = '2021, Florian Pfisterer'
 author = 'Florian Pfisterer'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
@@ -39,6 +42,7 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
@@ -71,7 +75,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -79,7 +83,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
