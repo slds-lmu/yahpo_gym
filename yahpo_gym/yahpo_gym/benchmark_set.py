0f9a4f30883ad98f093a99a0d0755f94176de724
pfistfl <pfistererf@googlemail.com>
2021-09-29 16:49:54 +0200

    json

--- yahpo_gym/yahpo_gym/benchmark_set.py
+++ yahpo_gym/yahpo_gym/benchmark_set.py
@@ -4,7 +4,7 @@ import time
 from pathlib import Path
 from typing import Union, Dict, List
 import numpy as np
-
+import json
 from ConfigSpace.read_and_write import json as CS_json
 import ConfigSpace as CS
 import ConfigSpace.hyperparameters as CSH
