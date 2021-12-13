093eaf0b722f75c5c37238cfcc1cf9a3c5d9bc47
Lennart Schneider <lennart.sch@web.de>
2021-12-13 14:46:00 +0100

    fix subset_codomain

--- yahpo_gym_r/R/BenchmarkSet.R
+++ yahpo_gym_r/R/BenchmarkSet.R
@@ -108,7 +108,7 @@ BenchmarkSet = R6::R6Class("BenchmarkSet",
     #'  A [`paradox::ParamSet`] containing the output space (codomain).
     subset_codomain = function(keep) {
       codomain = self$codomain
-      assert_choice(keep, names(codomain$params))
+      assert_subset(keep, names(codomain$params))
       new_domain = ParamSet$new(codomain$params[names(codomain$params) %in% keep])
       private$.domains$codomain = new_domain
     }
