b3c0011fc0baa69180b3c97a5cec4b90d5b23357
pfistfl <pfistererf@googlemail.com>
2022-01-26 20:44:30 +0100

    propagate seed, update notebook

--- yahpo_train/yahpo_train/models_ensemble.py
+++ yahpo_train/yahpo_train/models_ensemble.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     l = SurrogateEnsembleLearner(dls, f, loss_func=nn.MSELoss(reduction='mean'), metrics=nn.MSELoss)
     l.add_cb(MixHandler)
     l.fit_one_cycle(10, 1e-4)
-    l.export_onnx(cfg, 'cuda:0', suffix='_resnet_ensemble')
+    l.export_onnx(cfg, 'cuda:0', suffix='noisy')
 
 
 
