2296a4885b546c4a7640883418108d120713f17a
pfistfl <pfistererf@googlemail.com>
2022-01-27 09:01:58 +0100

    update

--- yahpo_train/yahpo_train/models_ensemble.py
+++ yahpo_train/yahpo_train/models_ensemble.py
@@ -12,16 +12,16 @@ from yahpo_train.models_utils import *
 from yahpo_train.models import AbstractSurrogate
 from yahpo_train.learner import SurrogateTabularLearner
 
-def sample_from_simplex(n):
+def sample_from_simplex(n, device):
     """
     Sample from a simplex.
     Following https://cs.stackexchange.com/questions/3227/uniform-sampling-from-a-simplex
     """
-    alpha = torch.rand(n+1)
+    alpha = torch.rand(n+1).to(device=device)
     alpha[0] = 0.0
-    alpha[-1] = 1.0
+    alpha[n] = 1.0
     alpha = torch.sort(alpha).values
-    return alpha[1:] - alpha[:-1]
+    return alpha[1:] - alpha[:n]
 
 class Ensemble(AbstractSurrogate):
     def __init__(self, base_model: nn.Module, n_models: int, **kwargs):
@@ -32,8 +32,7 @@ class Ensemble(AbstractSurrogate):
     def forward(self, x_cat, x_cont=None, invert_ytrafo=True) -> Tensor:
         ys = torch.stack([model(x_cat, x_cont, invert_ytrafo) for model in self.models], dim=0)
         # Draw ensemble weights
-        alpha = sample_from_simplex(self.n_models).to(ys.device)
-        
+        alpha = sample_from_simplex(self.n_models, ys.device)
         # Compute weighted average
         ys = ys * alpha[:,None, None]
         return torch.sum(ys, dim=0)
@@ -48,14 +47,14 @@ class SurrogateEnsembleLearner(SurrogateTabularLearner):
 
     def fit_one_cycle(self, n_epoch, lr_max=None, div=25., div_final=1e5, pct_start=0.25, wd=None, moms=None, cbs=None, reset_opt=False):
         for i in range(self.n_models):
-            print(f"Training ensemble model {i+1}/{self.n_models}")
+            lr_max = [0.5, 1., 2.][i] * lr_max
+            print(f"Training ensemble model {i+1}/{self.n_models} with lr {lr_max.tolist()[0]}")
             self.learners[i].fit_one_cycle(n_epoch, lr_max, div, div_final, pct_start, wd, moms, cbs, reset_opt)
 
 
 if __name__ == '__main__':
     import torch.nn as nn
     from yahpo_gym.configuration import cfg
-    from yahpo_gym.benchmarks import lcbench
     from yahpo_train.models import ResNet
     from yahpo_train.models_ensemble import *
     from yahpo_train.learner import SurrogateTabularLearner, dl_from_config
@@ -64,10 +63,11 @@ if __name__ == '__main__':
     dls = dl_from_config(cfg, nrows=None)
 
     print('Resnet:')
+    torch.manual_seed(4321)
     f = Ensemble(ResNet, n_models=3, dls=dls)
     l = SurrogateEnsembleLearner(dls, f, loss_func=nn.MSELoss(reduction='mean'), metrics=nn.MSELoss)
-    l.add_cb(MixHandler)
-    l.fit_one_cycle(1, 1e-4)
+    l.add_cb(MixHandler(alpha=.1))
+    l.fit_one_cycle(10, 1e-4)
     l.export_onnx(cfg, 'cuda:0', suffix='noisy')
 
 
