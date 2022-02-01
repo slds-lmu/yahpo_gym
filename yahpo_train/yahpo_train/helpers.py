1ffcfcba9e3c1ef8967a492eb7d3e971a19577e4
Lennart Schneider <lennart.sch@web.de>
2022-02-01 17:47:02 +0100

    feat: helper for generating test_set metrics as csv file

--- yahpo_train/yahpo_train/helpers.py
+++ yahpo_train/yahpo_train/helpers.py
@@ -6,7 +6,6 @@ import pandas as pd
 import torch
 
 def get_set_metrics(key, set="test", model=None, instance=None):
-    # note that this is extremely slow because we use the actual objective for prediction and batch prediction is simply a loop
     bench = benchmark_set.BenchmarkSet(key)
     if model is not None:
         bench.config.config.update({"model":model})
@@ -34,7 +33,18 @@ def get_set_metrics(key, set="test", model=None, instance=None):
     metrics = {"mae":mae, "r2":r2, "spearman":spearman}
     for metric_name,metric in zip(metrics.keys(), metrics.values()):
         values = metric(truth_tensor, response_tensor)
-        metrics_dict.update({metric_name:dict(zip([y + "_" + metric_name for y in bench.config.y_names], [*values]))})
+        metrics_dict.update({metric_name:dict(zip([y for y in bench.config.y_names], [*values]))})
 
     return metrics_dict
+
+def generate_all_test_set_metrics(key, model=None, save_to_csv=False):
+    bench = benchmark_set.BenchmarkSet(key)
+    test_set_metrics = pd.DataFrame.from_dict(get_set_metrics(key, model = model))
+    test_set_metrics["instance"] = "all"
+    for instance in bench.instances:
+        tmp = pd.DataFrame.from_dict(get_set_metrics(key, model = model, instance = instance))
+        tmp["instance"] = instance
+        test_set_metrics = pd.concat([test_set_metrics, tmp])
+    if save_to_csv:
+        test_set_metrics.to_csv(bench.config.config_path + "/test_set_metrics_" + model + ".csv", index_label = "target")
 
