f0e7344311d0c4a15d736613f8f2583703cf6f2c
Lennart Schneider <lennart.sch@web.de>
2022-02-02 14:43:11 +0100

    fix: cleanup tuning and fitting of resnet

---
+++ yahpo_train/notebooks/fit_resnet.py
@@ -0,0 +1,84 @@
+if __name__ == "__main__":
+
+    import optuna
+    import json
+    from tune_resnet import *
+    from yahpo_gym import benchmark_set
+    from yahpo_train.cont_scalers import *
+    from yahpo_train.helpers import generate_all_test_set_metrics
+    from functools import partial
+
+    # tfms_list holds for each benchmark scenario (key) optional transformers that should be fixed and not tuned
+    # taken from tune_resnet.py
+
+    tfms_list = {}
+
+    tfms_lcbench = {}
+    tfms_list.update({"lcbench":tfms_lcbench})
+
+    tfms_nb301 = {}
+    tfms_nb301.update({"epoch":partial(ContTransformerMultScalar, m=1/98)})
+    tfms_nb301.update({"val_accuracy":partial(ContTransformerMultScalar, m=1/100)})
+    tfms_nb301.update({"runtime":ContTransformerRange})
+    tfms_list.update({"nb301":tfms_nb301})
+
+    tfms_rbv2_super = {}  # FIXME:
+    tfms_list.update({"rbv2_super":tfms_rbv2_super})
+
+    tfms_rbv2_svm = {}  # FIXME:
+    tfms_list.update({"rbv2_svm":tfms_rbv2_svm})
+
+    tfms_rbv2_xgboost = {}  # FIXME:
+    tfms_list.update({"rbv2_xgboost":tfms_rbv2_xgboost})
+
+    tfms_rbv2_ranger = {}  # FIXME:
+    tfms_list.update({"rbv2_ranger":tfms_rbv2_ranger})
+
+    tfms_rbv2_rpart = {}  # FIXME:
+    tfms_list.update({"rbv2_rpart":tfms_rbv2_rpart})
+
+    tfms_rbv2_glmnet = {}  # FIXME:
+    tfms_list.update({"rbv2_glmnet":tfms_rbv2_glmnet})
+
+    tfms_rbv2_aknn = {}  # FIXME:
+    tfms_list.update({"rbv2_aknn":tfms_rbv2_aknn})
+
+    tfms_iaml_super = {}
+    [tfms_iaml_super.update({k:tfms_chain([ContTransformerInt, ContTransformerRange])}) for k in ["nf"]]
+    tfms_list.update({"iaml_super":tfms_iaml_super})
+
+    tfms_iaml_xgboost = {}
+    [tfms_iaml_xgboost.update({k:tfms_chain([ContTransformerInt, ContTransformerRange])}) for k in ["nf"]]
+    tfms_list.update({"iaml_xgboost":tfms_iaml_xgboost})
+
+    tfms_iaml_ranger = {}
+    [tfms_iaml_ranger.update({k:tfms_chain([ContTransformerInt, ContTransformerRange])}) for k in ["nf"]]
+    tfms_list.update({"iaml_ranger":tfms_iaml_ranger})
+
+    tfms_iaml_rpart = {}
+    [tfms_iaml_rpart.update({k:tfms_chain([ContTransformerInt, ContTransformerRange])}) for k in ["nf"]]
+    tfms_list.update({"iaml_rpart":tfms_iaml_rpart})
+
+    tfms_iaml_glmnet = {}
+    [tfms_iaml_glmnet.update({k:tfms_chain([ContTransformerInt, ContTransformerRange])}) for k in ["nf"]]
+    tfms_list.update({"iaml_glmnet":tfms_iaml_glmnet})
+    
+    keys = ["lcbench", "nb301", "iaml_glmnet", "iaml_rpart", "iaml_ranger", "iaml_xgboost", "iaml_super"]
+    for key in keys
+        bench = benchmark_set.BenchmarkSet(key)
+        cuda_available = torch.cuda.is_available()
+        if !cuda_available:
+            raise ValueError("No cuda device available. You probably do not want to fit on CPUs."
+        
+        storage_name = "sqlite:///{}.db".format("tune_" + key + "_resnet_test")
+        study = optuna.load_study("tune_" + key + "_resnet_test", storage_name)
+        best_params = study.best_params
+        with open(bench.config.config_path + "/best_params_resnet.json", "w") as f:
+            json.dump(json.dumps(best_params), f)
+        
+        # tfms see tfms_list above
+        l = fit_from_best_params_resnet(key, best_params=best_params, tfms_fixed=tfms_list.get(key), export=False, device="cuda:0")
+        l.export_onnx(cfg(key), device=torch, suffix="resnet")
+        
+        generate_all_test_set_metrics(key, model="new_model_resnet.onnx", save_to_csv=True)
+
--- yahpo_train/notebooks/tune_resnet.py
+++ yahpo_train/notebooks/tune_resnet.py
@@ -10,7 +10,7 @@ from functools import partial
 import wandb
 import argparse
 
-def fit_config_resnet(key, dls_train=None, save_df_test_encoding=True, embds_dbl=None, embds_tgt=None, tfms=None, lr=1e-4, epochs=100, d=256, d_hidden_factor=2., n_layers=4, hidden_dropout=0., residual_dropout=.2, bs=10240, frac=1., mixup=True, export=False, log_wandb=True, wandb_entity='mfsurrogates', cbs=[], device='cuda:0'):
+def fit_config_resnet(key, dls_train=None, save_df_test_encoding=True, embds_dbl=None, embds_tgt=None, tfms=None, lr=1e-4, epochs=100, d=256, d_hidden_factor=2., n_layers=4, hidden_dropout=0., residual_dropout=.2, bs=10240, frac=1., mixup=True, export=False, log_wandb=True, wandb_entity="mfsurrogates", cbs=[], device="cuda:0"):
     """
     Fit function with hyperparameters for resnet.
     """
@@ -27,7 +27,7 @@ def fit_config_resnet(key, dls_train=None, save_df_test_encoding=True, embds_dbl
 
     # Instantiate learner
     f = ResNet(dls_train, embds_dbl=embds_dbl, embds_tgt=embds_tgt, d=d, d_hidden_factor=d_hidden_factor, n_layers=n_layers, hidden_dropout=hidden_dropout, residual_dropout=residual_dropout)
-    l = SurrogateTabularLearner(dls_train, f, loss_func=nn.MSELoss(reduction='mean'), metrics=nn.MSELoss)
+    l = SurrogateTabularLearner(dls_train, f, loss_func=nn.MSELoss(reduction="mean"), metrics=nn.MSELoss)
     l.metrics = [AvgTfedMetric(mae), AvgTfedMetric(r2), AvgTfedMetric(spearman), AvgTfedMetric(napct)]
     if mixup:
         l.add_cb(MixHandler)
@@ -39,8 +39,8 @@ def fit_config_resnet(key, dls_train=None, save_df_test_encoding=True, embds_dbl
     if log_wandb:
         wandb.init(project=key, entity=wandb_entity)
         l.add_cb(WandbMetricsTableCallback())
-        wandb.config.update({'cont_tf': l.embds_dbl, 'tgt_tf': l.embds_tgt, 'fraction': frac,}, allow_val_change=True)
-        wandb.config.update({'deep': deep, 'deeper': deeper, 'dropout':dropout, 'wide':wide, 'use_bn':use_bn}, allow_val_change=True)
+        wandb.config.update({"cont_tf": l.embds_dbl, "tgt_tf": l.embds_tgt, "fraction": frac,}, allow_val_change=True)
+        wandb.config.update({"deep": deep, "deeper": deeper, "dropout":dropout, "wide":wide, "use_bn":use_bn}, allow_val_change=True)
 
     # Fit
     l.fit_flat_cos(epochs, lr)
@@ -78,9 +78,8 @@ def tune_config_resnet(key, name, tfms_fixed={}, trials=1000, walltime=86400, **
 
     trange = ContTransformerRange
     tlog = ContTransformerLogRange
-    tlog2 = ContTransformerLog2Range
     tnexp = ContTransformerNegExpRange
-    trafos = {"trange":trange, "tlog":tlog, "tlog2":tlog2, "tnexp":tnexp}
+    trafos = {"trange":trange, "tlog":tlog, "tnexp":tnexp}
 
     # for the search space see https://arxiv.org/pdf/2106.11959.pdf
 
@@ -91,7 +90,7 @@ def tune_config_resnet(key, name, tfms_fixed={}, trials=1000, walltime=86400, **
                 # if opt_tfms_y is False use ContTransformerRange
                 opt_tfms_y = trial.suggest_categorical("opt_tfms_" + y, [True, False])
                 if opt_tfms_y:
-                    tf = trial.suggest_categorical("tfms_" + y, ["tlog", "tlog2", "tnexp"])
+                    tf = trial.suggest_categorical("tfms_" + y, ["tlog", "tnexp"])
                 else:
                     tf = "trange"
                 tfms.update({y:trafos.get(tf)})
@@ -100,12 +99,12 @@ def tune_config_resnet(key, name, tfms_fixed={}, trials=1000, walltime=86400, **
                 # if opt_tfms_x is False use ContTransformerRange
                 opt_tfms_x = trial.suggest_categorical("opt_tfms_" + x, [True, False])
                 if opt_tfms_x:
-                    tf = trial.suggest_categorical("tfms_" + x, ["tlog", "tlog2", "tnexp"])
+                    tf = trial.suggest_categorical("tfms_" + x, ["tlog", "tnexp"])
                 else:
                     tf = "trange"
                 tfms.update({x:trafos.get(tf)})
 
-        d = trial.suggest_int("d", 64, 1024, step = 64)  # layer size
+        d = trial.suggest_int("d", 64, 512, step = 64)  # layer size
         d_hidden_factor = trial.suggest_float("d_hidden_factor", 1., 4.)  # hidden factor
         n_layers = trial.suggest_int("n_layers", 1, 8)  # number of layers
         hidden_dropout = trial.suggest_float("hidden_dropout", 0., 0.5)  # hidden dropout
@@ -116,7 +115,7 @@ def tune_config_resnet(key, name, tfms_fixed={}, trials=1000, walltime=86400, **
             residual_dropout = 0.
         lr = trial.suggest_float("lr", 1e-5, 1e-2, log=True)
         mixup = trial.suggest_categorical("mixup", [True, False])
-        cbs = [FastAIPruningCallback(trial=trial, monitor='valid_loss')]
+        cbs = [FastAIPruningCallback(trial=trial, monitor="valid_loss")]
         
         l = fit_config_resnet(key=key, dls_train=dls_train, tfms=tfms, lr=lr, d=d, d_hidden_factor=d_hidden_factor, n_layers=n_layers, hidden_dropout=hidden_dropout, residual_dropout=residual_dropout, mixup=mixup, log_wandb=False, cbs=cbs, **kwargs)
         loss = l.recorder.final_record.items[1]  # [1] is validation loss
@@ -133,9 +132,8 @@ def fit_from_best_params_resnet(key, best_params, tfms_fixed={}, log_wandb=False
 
     trange = ContTransformerRange
     tlog = ContTransformerLogRange
-    tlog2 = ContTransformerLog2Range
     tnexp = ContTransformerNegExpRange
-    trafos = {"trange":trange, "tlog":tlog, "tlog2":tlog2, "tnexp":tnexp}
+    trafos = {"trange":trange, "tlog":tlog, "tnexp":tnexp}
 
     for y in cc.y_names:
         if y not in tfms.keys():  # exclude variables provided in tfms_fixed
@@ -171,13 +169,13 @@ def fit_from_best_params_resnet(key, best_params, tfms_fixed={}, log_wandb=False
 
     return l
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # tfms_list holds for each benchmark scenario (key) optional transformers that should be fixed and not tuned
 
     tfms_list = {}
 
-    tfms_lcbench = {}  # FIXME:
+    tfms_lcbench = {}
     tfms_list.update({"lcbench":tfms_lcbench})
 
     tfms_nb301 = {}
@@ -227,13 +225,11 @@ if __name__ == '__main__':
     [tfms_iaml_glmnet.update({k:tfms_chain([ContTransformerInt, ContTransformerRange])}) for k in ["nf"]]
     tfms_list.update({"iaml_glmnet":tfms_iaml_glmnet})
 
-    # FIXME: fcnet, taskset
-
-    parser = argparse.ArgumentParser(description='Args for resnet tuning')
-    parser.add_argument('--key', type=str, default="iaml_glmnet", help='Key of benchmark scenario, e.g., "iaml_glmnet"')    
-    parser.add_argument('--name', type=str, default="tune_iaml_glmnet_resnet", help='Name of the optuna study, e.g., "tune_iaml_glmnet_resnet"')
-    parser.add_argument('--trials', type=int, default=0, help='Number of optuna trials')  # by default we run until terminated externally
-    parser.add_argument('--walltime', type=int, default=0, help='Walltime for optuna timeout in seconds') # by default we run until terminated externally
+    parser = argparse.ArgumentParser(description="Args for resnet tuning")
+    parser.add_argument("--key", type=str, default="iaml_glmnet", help="Key of benchmark scenario, e.g., "iaml_glmnet"")    
+    parser.add_argument("--name", type=str, default="tune_iaml_glmnet_resnet", help="Name of the optuna study, e.g., "tune_iaml_glmnet_resnet"")
+    parser.add_argument("--trials", type=int, default=0, help="Number of optuna trials")  # by default we run until terminated externally
+    parser.add_argument("--walltime", type=int, default=0, help="Walltime for optuna timeout in seconds") # by default we run until terminated externally
     args = parser.parse_args()
 
     cuda_available = torch.cuda.is_available()
@@ -245,50 +241,4 @@ if __name__ == '__main__':
         raise ValueError("No cuda device available. You probably do not want to tune on CPUs.")
 
     tune_config_resnet(args.key, name=args.name, tfms_fixed=tfms_list.get(args.key), trials=args.trials, walltime=args.walltime)
-
-#key = "lcbench"
-#bench = benchmark_set.BenchmarkSet(key)
-#cuda_available = torch.cuda.is_available()
-#
-#storage_name = "sqlite:///{}.db".format("tune_" + key + "_resnet_test")
-#study = optuna.load_study("tune_" + key + "_resnet_test", storage_name)
-#best_params = study.best_params
-#with open(bench.config.config_path + "/best_params_resnet.pkl", "wb") as f:
-#    pickle.dump(best_params, f)
-#
-## tfms see tfms_list in tune_resnet.py
-#l = fit_from_best_params_resnet(key, best_params=best_params, tfms_fixed=tfms_list.get(key), export=False, device="cuda:0")
-#l.export_onnx(cfg(key), device="cuda:0", suffix="resnet")
-#
-#from yahpo_train.helpers import generate_all_test_set_metrics
-#
-#generate_all_test_set_metrics(key, model="new_model.onnxresnet", save_to_csv=True)
-
-#if __name__ == '__main__':
-#    wandb.login()
-#    #device = torch.device("cpu")
-#    device = 'cuda:0'
-#    # general tuning example workflow
-#    # 1. specify transformers you want to consider fixed (and not tuned over)
-#    tfms_xgboost = {}
-#    tfms_xgboost.update({"nf":tfms_chain([ContTransformerInt, ContTransformerRange])})
-#    # 2. tune by providing the fixed transformers (if any)
-#    study_xgboost = tune_config_resnet("iaml_xgboost", name="tune_iaml_xgboost_new", tfms_fixed=tfms_xgboost)
-#    # 3. extract the best params and refit the model (FIXME: could refit on whole train + valid data?)
-#    #    set export = True so that the onnx model is exported
-#    #    caveat: this overwrites the exiting model! # FIXME: should versionize this automatically
-#    l = fit_from_best_params_resnet("iaml_xgboost", best_params=study_xgboost.best_params, tfms_fixed=tfms_xgboost, export=True, device=device, epochs=100)
-#    # 4. get the performance metrics on the test set relying on the newly exported onnx model
-#    get_testset_metrics("iaml_xgboost")
-#
-#    # load existing one:
-#    name = storage_name = "sqlite:///{}.db".format("tune_iaml_xgboost_new")
-#    study_xgboost = optuna.load_study(None, storage = name)
-#
-#    # tfms_super nf tfms_chain([ContTransformerInt, ContTransformerRange]
-#    tfms_super.update({"timetrain":ContTransformerLogRange})
-#    tfms_super.update({"timepredict":ContTransformerLogRange})
-#    tfms_super.update({"rammodel":ContTransformerLogRange})
-#    tfms_super.update({"ias":ContTransformerLogRange})
-#    fit_from_best_params_resnet("iaml_super", study_super.best_params, tfms_fixed=tfms_super, log_wandb=True, export=True, epochs=100)
 
