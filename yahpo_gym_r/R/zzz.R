213c3b2ed7c719be1aa636edceaf53c87700f91d
Lennart Schneider <lennart.sch@web.de>
2023-04-23 19:16:21 +0200

    R chore

--- yahpo_gym_r/R/zzz.R
+++ yahpo_gym_r/R/zzz.R
@@ -4,6 +4,7 @@
 #' @import bbotk
 #' @import R6
 #' @import data.table
+#' @importFrom stats setNames
 #' @description
 #' A package that connects YAHPO Gym to R.
 "_PACKAGE"
@@ -11,5 +12,8 @@
 .onLoad = function(libname, pkgname) { # nocov start
     reticulate::configure_environment(pkgname)
 } # nocov end
+
+# static code checks should not complain about commonly used data.table columns
+utils::globalVariables(c("search_space", "domain", "codomain", "quant"))
 
 leanify_package()
