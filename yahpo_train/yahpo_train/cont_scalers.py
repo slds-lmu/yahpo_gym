8d15f064a4c1ce922fd793f1944f374f5a18787e
Lennart Schneider <lennart.sch@web.de>
2023-05-07 01:43:58 +0200

    ..

--- yahpo_train/yahpo_train/cont_scalers.py
+++ yahpo_train/yahpo_train/cont_scalers.py
@@ -115,9 +115,9 @@ class ContTransformerStandardizeGrouped(nn.Module):
         Batch-wise transform for x.
         """
         current_device = x.device
-        group.to_device(current_device)
-        self.means.to_device(current_device)
-        self.sds.to_device(current_device)
+        group.to(current_device)
+        self.means.to(current_device)
+        self.sds.to(current_device)
         means = self.means.index_select(dim=0, index=group - 1)
         sds = self.sds.index_select(dim=0, index=group - 1)
         x = (x - means) / sds
@@ -128,9 +128,9 @@ class ContTransformerStandardizeGrouped(nn.Module):
         Batch-wise inverse transform for x.
         """
         current_device = x.device
-        group.to_device(current_device)
-        self.means.to_device(current_device)
-        self.sds.to_device(current_device)
+        group.to(current_device)
+        self.means.to(current_device)
+        self.sds.to(current_device)
         means = self.means.index_select(dim=0, index=group - 1)
         sds = self.sds.index_select(dim=0, index=group - 1)
         x = x * sds + means
